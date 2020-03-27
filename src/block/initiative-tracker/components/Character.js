de8999701b91bfe56f96f060c5cd3c24b00170d6
R A Van Epps <rave@ravanepps.com>
2020-03-27 15:35:09 -0600

    Update Character component

--- src/block/initiative-tracker/components/Character.js
+++ src/block/initiative-tracker/components/Character.js
@@ -5,13 +5,48 @@ const {
     i18n: {
         __,
     },
+    element: {
+        Component,
+    },
 } = wp;
 
-const Character = ( props ) => {
-	// console.log( 'props', props )
-	return (
-		<p>Char</p>
-	);
-}
+export default class Character extends Component {
+	constructor( props ) {
+		super( props )
+	
+		this.state = {
+			editing: false,
+		}
+	}
 
-export default Character;
+	render() {
+		const {
+			editing,
+		} = this.state;
+		const {
+			characterName,
+			playerName,
+			type,
+		} = this.props;
+
+		{ editing && (
+			<div className="edit-character">
+				<TextControl
+					label={ __( 'Character Name', 'rave-rpg-initiative' ) }
+				/>
+				{ 'player' === type && (
+					<TextControl
+						label={ __( 'Player Name', 'rave-rpg-initiative' ) }
+					/>
+				) }
+			</div>
+		) }
+
+		return (
+			<div className="character">
+				<span className="character-name">{ characterName }</span>
+				<span>{ ` - ( ${type} )` }</span>
+			</div>
+		);
+	}
+}
