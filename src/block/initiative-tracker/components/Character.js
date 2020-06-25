81f39a2a406455873b3af89dbda57d95bd8de3a3
R A Van Epps <rave@ravanepps.com>
2020-06-25 12:57:19 -0600

    Remove useEffect

--- src/block/initiative-tracker/components/Character.js
+++ src/block/initiative-tracker/components/Character.js
@@ -13,7 +13,6 @@ const {
 	},
     element: {
         Component,
-        useEffect,
         useState,
     },
 } = wp;
--- src/block/initiative-tracker/components/CharacterList.js
+++ src/block/initiative-tracker/components/CharacterList.js
@@ -8,7 +8,6 @@ const {
     },
     element: {
         Component,
-        useEffect,
         useState,
     },
 } = wp;
