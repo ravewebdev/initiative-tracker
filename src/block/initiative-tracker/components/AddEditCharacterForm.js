f6145716673f75806912a3d3c8127054facab051
R A Van Epps <rave@ravanepps.com>
2020-06-25 12:15:54 -0600

    Convert add/edit form to functional component

--- src/block/initiative-tracker/components/AddEditCharacterForm.js
+++ src/block/initiative-tracker/components/AddEditCharacterForm.js
@@ -12,135 +12,129 @@ const {
     },
     element: {
         Component,
+        useState,
+        useEffect,
     },
 } = wp;
 
-export default class AddEditCharacterForm extends Component {
-    constructor( props ) {
-        super( props )
-    
-        this.state = {
-            name: null,
-            player: null,
-            initiative: 0,
-            nameIsEmpty: true,
-            playerIsEmpty: true,
-            editing: false,
-            index: null,
-        };
-    };
+const AddEditCharacterForm = ( props ) => {
+    const {
+        type,
+        characterFn,
+        toggle,
+        character,
+    } = props;
 
-    componentDidMount() {
-        // Populate existing character values when in edit mode.
-        if ( this.props.hasOwnProperty( 'character' ) ) {
-            const {
-                character: {
-                    name,
-                    player,
-                    initiative,
-                    index,
-                },
-                type,
-            } = this.props;
+    const [ state, setState ] = useState( {
+        name: null,
+        player: null,
+        initiative: 0,
+        nameIsEmpty: true,
+        playerIsEmpty: true,
+        editing: false,
+        index: null,
+    } );
 
-            this.setState( {
-                nameIsEmpty: false,
-                playerIsEmpty: false,
-                editing: true,
-                name,
-                player,
-                index,
-                initiative
-            } );
+    // Set initial state values.
+    useEffect( () => {
+        if ( undefined === character ) {
+            return;
         }
-    };
 
-    render() {
-        const {
-            name,
-            player,
-            initiative,
-            nameIsEmpty,
-            playerIsEmpty,
-            editing,
-            index,
-        } = this.state;
-        const {
-            type,
-            characterFn,
-            toggle,
-        } = this.props;
-        const errorClass = 'input-error';
-        const isPlayer = 'player' === type;
-        const disableSave = ( isPlayer && playerIsEmpty ) || nameIsEmpty;
+        setState( {
+            ...state,
+            ...character,
+            nameIsEmpty: false,
+            playerIsEmpty: false,
+            editing: true,
+        } );
+    }, [] );
 
-        return (
-            <div className="edit-character-form">
+    const {
+        name,
+        player,
+        initiative,
+        nameIsEmpty,
+        playerIsEmpty,
+        editing,
+        index,
+    } = state;
+
+    const errorClass = 'input-error',
+        isPlayer = 'player' === type,
+        disableSave = ( isPlayer && playerIsEmpty ) || nameIsEmpty;
+
+    return (
+        <div className="edit-character-form">
+            <TextControl
+                label={ __( 'Character Name *', 'initiative-tracker' ) }
+                value={ nameIsEmpty ? '' : name }
+                onChange={ ( name ) => {
+                    setState( {
+                        ...state,
+                        name,
+                        nameIsEmpty: ( "" === name.trim() ),
+                    } );
+                } }
+                className={ null !== name && nameIsEmpty ? errorClass : '' }
+            />
+            { isPlayer && (
                 <TextControl
-                    label={ __( 'Character Name *', 'initiative-tracker' ) }
-                    value={ name }
-                    onChange={ ( name ) => {
-                        this.setState( {
-                            name,
-                            nameIsEmpty: ( "" === name.trim() )
+                    label={ __( 'Player Name *', 'initiative-tracker' ) }
+                    value={ playerIsEmpty ? '' : player }
+                    onChange={ ( player ) => {
+                        setState( {
+                            ...state,
+                            player,
+                            playerIsEmpty: ( "" === player.trim() ),
                         } );
                     } }
-                    className={ null !== name && nameIsEmpty ? errorClass : '' }
+                    className={ null !== player && playerIsEmpty ? errorClass : '' }
                 />
-                { isPlayer && (
-                    <TextControl
-                        label={ __( 'Player Name *', 'initiative-tracker' ) }
-                        value={ player }
-                        onChange={ ( player ) => {
-                            this.setState( {
-                                player,
-                                playerIsEmpty: ( "" === player.trim() )
+            ) }
+            <TextControl
+                label={ __( 'Initiative', 'initiative-tracker' ) }
+                type="number"
+                value={ initiative }
+                onChange={ ( initiative ) => {
+                    setState( {
+                        ...state,
+                        initiative,
+                    } );
+                } }
+            />
+            <div className="edit-character-buttons">
+                <Button
+                    isSecondary
+                    onClick={ toggle }
+                >
+                    { __( 'Cancel', 'initiative-tracker' ) }
+                </Button>
+                <Button
+                    isPrimary
+                    disabled={ disableSave }
+                    onClick={ () => {
+                        if ( editing ) {
+                            characterFn( type, index, {
+                                name: name.trim(),
+                                player: ( isPlayer ? player.trim() : '' ),
+                                initiative,
                             } );
-                        } }
-                        className={ null !== player && playerIsEmpty ? errorClass : '' }
-                    />
-                ) }
-                <TextControl
-                    label={ __( 'Initiative', 'initiative-tracker' ) }
-                    type="number"
-                    value={ initiative }
-                    onChange={ ( initiative ) => {
-                        this.setState( {
-                            initiative
-                        } )
+                        } else {
+                            characterFn( type, {
+                                name: name.trim(),
+                                player: ( isPlayer ? player.trim() : '' ),
+                                initiative,
+                            } );
+                        }
+                        toggle();
                     } }
-                />
-                <div className="edit-character-buttons">
-                    <Button
-                        isSecondary
-                        onClick={ toggle }
-                    >
-                        { __( 'Cancel', 'initiative-tracker' ) }
-                    </Button>
-                    <Button
-                        isPrimary
-                        disabled={ disableSave }
-                        onClick={ () => {
-                            if ( editing ) {
-                                characterFn( type, index, {
-                                    name: name.trim(),
-                                    player: ( isPlayer ? player.trim() : '' ),
-                                    initiative,
-                                } );
-                            } else {
-                                characterFn( type, {
-                                    name: name.trim(),
-                                    player: ( isPlayer ? player.trim() : '' ),
-                                    initiative,
-                                } );
-                            }
-                            toggle();
-                        } }
-                    >
-                        { __( 'Save', 'initiative-tracker' ) }
-                    </Button>
-                </div>
+                >
+                    { __( 'Save', 'initiative-tracker' ) }
+                </Button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default AddEditCharacterForm;
