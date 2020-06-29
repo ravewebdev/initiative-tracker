99596c80b35c2e57f82411a4e22717d96d260eac
R A Van Epps <rave@ravanepps.com>
2020-06-29 16:51:49 -0600

    Add fn comment

--- src/block/initiative-tracker/save.js
+++ src/block/initiative-tracker/save.js
@@ -2,6 +2,15 @@
  * SAVE: Initiative Tracker Block
  */
 
+/**
+ * Handle save functionality in the admin.
+ *
+ * @author R A Van Epps <rave@ravanepps.com>
+ * @since  1.0.0
+ *
+ * @param  {Object} props  Block props.
+ * @return {?ReactElement} Block save JSX.
+ */
 const Save = ( props ) => {
     return null;
 };
